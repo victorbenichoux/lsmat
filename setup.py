ad8928097e23c355266274b38eb42a38e28f893d
victorbenichoux <victor.benichoux@pasteur.fr>
2018-03-22 10:25:35 +0100

    Initial commit

--- setup.py
+++ setup.py
@@ -1,21 +1,12 @@
 from setuptools import *
-__author__ = 'victor'
+__author__ = 'victorbenichoux'
 
 setup(
-    name = 'spikesorting_cmdline',
+    name = 'lsmat',
     version = '0.1',
     author = 'Victor Benichoux',
     author_email = 'victor.benichoux@gmail',
-    packages = ['spikesorting_cmdlinetools',
-                "spikesorting_cmdlinetools.utils"],
-    entry_points = {
-        'console_scripts' : ['sorting-manager=spikesorting_cmdlinetools.sorting_manager:main',
-                             'lsmat=spikesorting_cmdlinetools.utils.lsmat:main',
-                             'sorting-manager-putback=spikesorting_cmdlinetools.put_back_in_matfiles:main',
-                             'update_prmfiles=spikesorting_cmdlinetools.utils.update_prmfiles:main']
-    },
-    package_data={
-        'spikesorting_cmdlinetools': ['probe files/*.prb']
-    }
-
+    packages = ['lsmat'],
+    entry_points = dict(console_scripts = ['lsmat=lsmat.lsmat:main']),
+    package_data=dict()
 )
